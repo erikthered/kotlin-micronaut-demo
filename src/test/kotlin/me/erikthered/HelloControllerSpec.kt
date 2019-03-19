4bbf8600a10b4af9eb4a5aab30b60b9c84d685f6
Erik Nelson <erik.david.nelson@gmail.com>
2019-03-19 15:24:58 -0400

    Remove JUnit 4 and Hamcrest
    Replace with Strikt

--- src/test/kotlin/me/erikthered/HelloControllerSpec.kt
+++ src/test/kotlin/me/erikthered/HelloControllerSpec.kt
@@ -3,11 +3,11 @@ package me.erikthered
 import io.micronaut.context.ApplicationContext
 import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
-import org.hamcrest.Matchers
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.junit.Assert
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 
 object HelloControllerSpec: Spek({
     describe("HelloController suite") {
@@ -16,7 +16,7 @@ object HelloControllerSpec: Spek({
 
         it("test /hello responds with 'Hello World'") {
             val resp = client.toBlocking().retrieve("/hello")
-            Assert.assertThat(resp, Matchers.comparesEqualTo("Hello World"))
+            expectThat(resp).isEqualTo("Hello World")
         }
 
         afterGroup {
